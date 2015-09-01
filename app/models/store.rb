79f16b8f3c92f573c9ce2e2d90497070a1009bc5
Tatiana Ivanova <tanyavka90@gmail.com>
2015-09-01 18:55:49 +0300

    [ROLES & MASK] mask done

--- app/models/store.rb
+++ app/models/store.rb
@@ -5,7 +5,8 @@ class Store
   field :title, type: String
 
   field :name, type: String
-  field :type, type: String
+  field :speciality, type: String
+  field :goods_type, type: String
   field :date_of_opening, type: Date
   field :store_country, type: String
   field :store_city, type: String
@@ -13,8 +14,8 @@ class Store
   field :last_buying_at, type: DateTime
   field :buying_count, type: Integer
 
-  validates :name, :type, :store_country, :store_city, presence: true
-  validates :title, uniqueness: true
+  validates :title, :name, :speciality, :goods_type, :store_country, :store_city, presence: true
+  validates :title, uniqueness: true, unless: 'title.nil?'
 
   belongs_to :user
   has_many :goods, dependent: :destroy
